a95374c64cde63501f7988ff6a79646aa2722b02
Alihan Dzhumaev <alihandzhumaev13@gmail.com>
2021-07-27 11:49:05 +0600

    java_script

---
+++ script.js
@@ -0,0 +1,23 @@
+$(function () {
+
+    //задание 1
+    const phoneNumber = $('.number');
+
+    const footer = $(".footer");
+
+    const numberCopy = phoneNumber.clone();
+
+    footer.append(numberCopy);
+
+    //задание 2
+
+    tel.addClass("border")
+
+
+
+});
+
+//     phoneNumber.each(function () {
+//         $(this).css("border", "1px solid black");
+//     })
+//
